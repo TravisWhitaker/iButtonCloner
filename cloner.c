151b4d01fc5cb946f0186d5262817d30e03cf935
Travis Whitaker <pi.boy.travis@gmail.com>
2013-05-12 16:27:53 -0400

    Defined shit, sending bits works.

--- cloner.c
+++ cloner.c
@@ -2,38 +2,86 @@
 #include <avr/pgmspace.h>
 #include <util/delay.h>
 
+//iButton is on pin D0.
+
+#define drivePin DDRD |= (1<<0)
+#define driveLow PORTD &= ~(1<<0)
+#define floatPin DDRD &= ~(1<<0)
+#define driveStatus DDRD |= (1<<6)
+#define statusOn PORTD |= (1<<6)
+#define statusOff PORTD &= ~(1<<6)
+
+
+
+void send1()
+{
+	drivePin;
+	driveLow;
+	_delay_us(15);
+	floatPin;
+	_delay_us(45);
+	return;
+}
+
+void send0()
+{
+	drivePin;
+	driveLow;
+	_delay_us(60);
+	floatPin;
+	return;
+}
+
+int resetPresence()
+{
+	drivePin;
+	driveLow;
+	_delay_us(510);
+	floatPin;
+	_delay_us(5);
+	if(!(PIND & (1<<0)))
+	{
+		return 0;
+	}
+	_delay_us(60);
+	if(PIND & (1<<0))
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
 int main()
 {
 	CLKPR = 0x80;
 	CLKPR = 0x00; //16MHz, 16 cycles per microsecond
-	DDRD |= (1<<6);
-	DDRD &= ~(1<<0);
-	PORTD |= (1<<6);
+	floatPin;
+	driveStatus;
+	statusOn;
 	_delay_ms(250);
-	PORTD &= ~(1<<6);
+	statusOff;
 	_delay_ms(250);
-	PORTD |= (1<<6);
+	statusOn;
 	_delay_ms(250);
-	PORTD &= ~(1<<6);
+	statusOff;
+	int status = 0;
 	while(1)
 	{
-		DDRD |= (1<<0);
-		PORTD &= ~(1<<0);
-		_delay_us(510);
-		DDRD &= ~(1<<0);
-		PORTD &= ~(1<<0);
-		_delay_us(65);
-		if(PIND & (1<<0))
+		status = resetPresence();
+		if(status == 0)
 		{
-			PORTD &= ~(1<<6);
+			statusOff;
 		}
 		else
 		{
-			PORTD |= (1<<6);
+			statusOn;
 			_delay_ms(250);
-			PORTD &= ~(1<<6);
+			statusOff;
 		}
-	_delay_ms(100);
+		_delay_ms(100);
 	}
 	return 0;
 }
